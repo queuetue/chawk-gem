fcdcc1896ebd5ef7a81f5af8c3f1616efb0fe65d
Scott Russell <queuetue@gmail.com>
2014-03-09 14:37:50 -0400

    Basic functionality tested (local array)

--- lib/chawk.rb
+++ lib/chawk.rb
@@ -3,20 +3,40 @@ require "chawk/version"
 module Chawk
 	class Pointer
 	  attr_accessor :path
-	  def initialize(*path)
+	  def initialize(path)
+		unless path.is_a?(Array)
+			raise ArgumentError
+			self.delete
+		end
+
+		unless path.reject{|x|x.is_a?(String)}.empty?
+			raise ArgumentError
+			self.delete
+		end
+
+		unless path.select{|x|x.include?('/')}.empty?
+			raise ArgumentError
+			self.delete
+		end
+
 	    @path = path
+	    #puts "PATH: #{path}"
 	    @history = (1..25).collect{|x|rand(100)}
 	  end
+
+
 	  def address
 	    @path.join("/")
 	  end
 
-	  def <<(*args)
+	  def <<(args)
 	  	if args.is_a?(Array)
+			#puts "ARGS: #{args}"
 	  		args.each do |arg|
 	  			if arg.is_a?(Integer)
 	  				@history << arg
 	  			else
+	  				#puts "ARGS/ARG ERROR: #{arg}"
 	  				raise ArgumentError
 	  			end
 	  		end
@@ -24,6 +44,7 @@ module Chawk
 			if args.is_a?(Integer)
 	  			@history << args
 			else
+	  			#puts "ARG ERROR: #{arg}"
   				raise ArgumentError
   			end
 	  	end
@@ -31,6 +52,22 @@ module Chawk
 
 	  def last
 	  	return @history[-1]
+	  end
+
+	  def clear_history!
+	  	@history.clear
+	  end
+
+	  def length
+	  	@history.length
+	  end
+
+	  def max
+	  	@history.max
+	  end
+
+	  def min
+	  	@history.min
 	  end
 
 	end
--- spec/lib/chawk_spec.rb
+++ spec/lib/chawk_spec.rb
@@ -4,7 +4,7 @@ describe Chawk do
 	pointer = nil
 	before :each do
     	pointer = Chawk::Pointer.new(['a','b'])
-    	puts pointer
+    	#puts pointer
     end
 
 	it "has address()" do
@@ -13,8 +13,15 @@ describe Chawk do
 
 	it "address is a/b" do
  		pointer.address.should eq("a/b")
-    	Chawk::Pointer.new(['0','x','z']).address.should eq("0/x/z")
-	end
+	   	Chawk::Pointer.new(['0','x','z']).address.should eq("0/x/z")
+ 	end
+
+ 	it "rejects invalid paths" do
+		lambda {Chawk::Pointer.new('A')}.should raise_error()
+		lambda {Chawk::Pointer.new(0)}.should raise_error()
+		lambda {Chawk::Pointer.new(['/','x','z'])}.should raise_error()
+		lambda {Chawk::Pointer.new(['a/a','x','z'])}.should raise_error()
+ 	end
 
 	it "accepts <<" do
 		pointer.should respond_to(:"<<")
@@ -25,12 +32,12 @@ describe Chawk do
 		lambda {pointer << 0}.should_not raise_error()
 		lambda {pointer << 190}.should_not raise_error()
 		lambda {pointer << 10000}.should_not raise_error()
+		lambda {pointer << [10,0,190,10000]}.should_not raise_error()
 	end
 
 	it "only accepts integers" do
 		lambda {pointer << 10.0}.should raise_error()
 		lambda {pointer << nil}.should raise_error()
-		lambda {pointer << []}.should raise_error()
 	end
 
 	it "has last()" do
@@ -44,6 +51,66 @@ describe Chawk do
  		pointer.last.should eq(1000)
 		pointer << 99
  		pointer.last.should eq(99)
+		pointer << [10,0,190,10000]
+ 		pointer.last.should eq(10000)
+	end
+
+	it "has clear_history!()" do
+ 		pointer.should respond_to(:clear_history!)
+	end
+
+	it "has length()" do
+ 		pointer.should respond_to(:length)
+	end
+
+	it "clears history" do
+		lambda {pointer.clear_history!}.should_not raise_error()
+		pointer.length.should eq(0)
+	end
+
+	it "calculates length" do
+		pointer.clear_history!
+		pointer.length.should eq(0)
+		pointer << 2
+		pointer.length.should eq(1)
+		pointer << 2
+		pointer.length.should eq(2)
+		pointer << 2
+		pointer.length.should eq(3)
+		pointer << [1,2,3,4]
+		pointer.length.should eq(7)
+	end
+
+	it "has max()" do
+ 		pointer.should respond_to(:max)
+	end
+
+	it "does max()" do
+		pointer.clear_history!
+		pointer << [1,2,3,4,5]
+		pointer.max.should eq(5)
+		pointer << 100
+		pointer.max.should eq(100)
+		pointer << 100
+		pointer.max.should eq(100)
+		pointer << 99
+		pointer.max.should eq(100)
+		pointer << 0
+		pointer.max.should eq(100)
+	end
+
+	it "does min()" do
+		pointer.clear_history!
+		pointer << [11,12,13,14,15]
+		pointer.min.should eq(11)
+		pointer << 100
+		pointer.min.should eq(11)
+		pointer << 10
+		pointer.min.should eq(10)
+		pointer << 99
+		pointer.min.should eq(10)
+		pointer << 0
+		pointer.min.should eq(0)
 	end
 
 end
