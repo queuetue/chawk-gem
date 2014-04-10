73c497d0ccb9cc9220cae7d9c90820b481f5d828
Scott Russell <queuetue@gmail.com>
2014-04-10 07:43:49 -0400

    Clean, tests pass.

--- test/schema.rb
+++ test/schema.rb
@@ -12,73 +12,73 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "chawk_agents", force: true do |t|
-    t.integer  "foreign_id"
-    t.string   "name",       limit: 200
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'chawk_agents', force: true do |t|
+    t.integer 'foreign_id'
+    t.string 'name',       limit: 200
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "chawk_nodes", force: true do |t|
-    t.string  "key",          limit: 150
-    t.text    "decription"
-    t.boolean "public_read",              default: false
-    t.boolean "public_write",             default: false
+  create_table 'chawk_nodes', force: true do |t|
+    t.string 'key',          limit: 150
+    t.text 'decription'
+    t.boolean 'public_read',              default: false
+    t.boolean 'public_write',             default: false
   end
 
-  create_table "chawk_points", force: true do |t|
-    t.float    "observed_at"
-    t.datetime "recorded_at"
-    t.text     "meta"
-    t.integer  "value"
-    t.integer  "node_id",     null: false
+  create_table 'chawk_points', force: true do |t|
+    t.float 'observed_at'
+    t.datetime 'recorded_at'
+    t.text 'meta'
+    t.integer 'value'
+    t.integer 'node_id',     null: false
   end
 
-  add_index "chawk_points", ["node_id"], name: "index_chawk_points_node"
+  add_index 'chawk_points', ['node_id'], name: 'index_chawk_points_node'
 
-  create_table "chawk_ranges", force: true do |t|
-    t.string   "subkey"
-    t.integer  "parent_node_id"
-    t.integer  "data_node_id"
-    t.float    "start_ts"
-    t.float    "stop_ts"
-    t.integer  "beats"
-    t.integer  "default"
-    t.string   "strategy"
-    t.datetime "expires"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'chawk_ranges', force: true do |t|
+    t.string 'subkey'
+    t.integer 'parent_node_id'
+    t.integer 'data_node_id'
+    t.float 'start_ts'
+    t.float 'stop_ts'
+    t.integer 'beats'
+    t.integer 'default'
+    t.string 'strategy'
+    t.datetime 'expires'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "chawk_relations", force: true do |t|
-    t.boolean "admin",    default: false
-    t.boolean "read",     default: false
-    t.boolean "write",    default: false
-    t.integer "agent_id",                 null: false
-    t.integer "node_id",                  null: false
+  create_table 'chawk_relations', force: true do |t|
+    t.boolean 'admin',    default: false
+    t.boolean 'read',     default: false
+    t.boolean 'write',    default: false
+    t.integer 'agent_id',                 null: false
+    t.integer 'node_id',                  null: false
   end
 
-  add_index "chawk_relations", ["agent_id"], name: "index_chawk_relations_agent"
-  add_index "chawk_relations", ["node_id"], name: "index_chawk_relations_node"
+  add_index 'chawk_relations', ['agent_id'], name: 'index_chawk_relations_agent'
+  add_index 'chawk_relations', ['node_id'], name: 'index_chawk_relations_node'
 
-  create_table "chawk_usage_records", force: true do |t|
-    t.integer  "agent_id"
-    t.integer  "node_id"
-    t.string   "description"
-    t.float    "time"
-    t.float    "space"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table 'chawk_usage_records', force: true do |t|
+    t.integer 'agent_id'
+    t.integer 'node_id'
+    t.string 'description'
+    t.float 'time'
+    t.float 'space'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
   end
 
-  create_table "chawk_values", force: true do |t|
-    t.float    "observed_at"
-    t.datetime "recorded_at"
-    t.text     "meta"
-    t.text     "value"
-    t.integer  "node_id",     null: false
+  create_table 'chawk_values', force: true do |t|
+    t.float 'observed_at'
+    t.datetime 'recorded_at'
+    t.text 'meta'
+    t.text 'value'
+    t.integer 'node_id',     null: false
   end
 
-  add_index "chawk_values", ["node_id"], name: "index_chawk_values_node"
+  add_index 'chawk_values', ['node_id'], name: 'index_chawk_values_node'
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@
 require 'simplecov'
 
 SimpleCov.start do
-  add_filter "/test/"
+  add_filter '/test/'
 end
 
 require 'chawk'
@@ -20,29 +20,28 @@ ENV['RACK_ENV'] = 'test'
 WORKING_DIRECTORY = Dir.pwd.freeze
 ARGV.clear
 
-ENV["CHAWK_DEBUG"] ? debug_level=ENV["CHAWK_DEBUG"] : debug_level=:info
+ENV['CHAWK_DEBUG'] ? debug_level = ENV['CHAWK_DEBUG'] : debug_level = :info
 
-#if ENV["TEST_DATABASE_LOG"]
+# if ENV["TEST_DATABASE_LOG"]
 #  ActiveRecord::Base.logger = Logger.new(STDOUT)
-#end
+# end
 
 require 'active_record'
 require 'yaml'
 
-if ENV["TEST_DATABASE_URLX"]
-  ActiveRecord::Base.establish_connection ENV["TEST_DATABASE_URL"]
+if ENV['TEST_DATABASE_URLX']
+  ActiveRecord::Base.establish_connection ENV['TEST_DATABASE_URL']
 else
-  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+  ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
   ActiveRecord::Migration.verbose = false
-  require "chawk/migration"
+  require 'chawk/migration'
   CreateChawkBase.migrate :up
   CreateChawkBase.migrate :down # I'm surprised what I'll do to increase coverage.
   CreateChawkBase.migrate :up
-  File.open('./test/schema.rb', "w") do |file|
+  File.open('./test/schema.rb', 'w') do |file|
     ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, file)
   end
 end
-
 
 load File.dirname(__FILE__) + '/schema.rb'
 require File.dirname(__FILE__) + '/../lib/models.rb'
